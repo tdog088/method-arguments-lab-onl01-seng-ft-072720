60d9cb4c5d29ffc53790e07cff1dba1e335cd998
Taliyah Wilson <taliyahwilson27@gmail.com>
2020-07-29 04:38:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-dev introduction(name)
+def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-dev introduction_with_language(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
